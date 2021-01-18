c26ef2d010376b260d7998eda04ef4d951aefd11
assoft <macsonfor@gmail.com>
2021-01-18 16:15:14 +0300

    initial

---
+++ README.md
@@ -0,0 +1,50 @@
+<h1 align="center">My team page</h1>
+
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
+
+<div align="center">
+  <h3>
+    <a href="https://{your-demo-link.your-domain}">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://{your-url-to-the-solution}">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/hhmesazsqgKXrTkYkt0U">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [Features](#features)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+
+## Overview
+
+![screenshot](https://raw.githubusercontent.com/assoft/devchallenges.io-my-team-page/main/my-team-page.jpg)
+
+### Built With
+
+- [Vitejs](https://vitejs.dev/)
+- [Vue.js](https://vuejs.org/)
+- [Tailwind](https://tailwindcss.com/)
+
+## Features
+
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/hhmesazsqgKXrTkYkt0U) was to build an application to complete the given user stories.
+
+## Contact
+- GitHub [@assoft](https://{github.com/assoft})
+- Twitter [@assoftTR](https://{twitter.com/assoftTR})
---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <div class="container mx-auto max-w-5xl p-8">
+    <div class="flex flex-col md:flex-row mb-24 gap-12">
+      <div class="w-full">
+        <h3 class="text-4xl font-bold font-pt-serif">The creative crew</h3>
+      </div>
+      <div class="flex flex-col w-full text-lg font-poppins gap-2">
+        <h4 class="font-bold">WHO WE ARE</h4>
+        <p class="max-w-md">
+          We are team of creatively diverse. driven. innovative individuals
+          working in various locations from the world.
+        </p>
+      </div>
+    </div>
+    <div
+      class="card-container grid grid-cols-2 md:grid-cols-3 grid-rows-3 md:grid-rows-2 gap-x-16"
+    >
+      <div class="block">
+        <div
+          class="transform transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer"
+        >
+          <div class="card flex">
+            <div class="flex-1 aspect-w-4 aspect-h-5">
+              <img
+                class="object-cover object-top"
+                src="./assets/photo1.jpg"
+                alt="user"
+              />
+            </div>
+            <div class="card-position flex-shrink-0 pl-1">
+              <span class="text-xs font-pt-serif uppercase">Product Owner</span>
+            </div>
+          </div>
+          <h4 class="text-lg md:text-xl font-semibold mt-2">Bill Mahoney</h4>
+        </div>
+      </div>
+
+      <div class="block">
+        <div
+          class="transform transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer"
+        >
+          <div class="card flex">
+            <div class="flex-1 aspect-w-4 aspect-h-5">
+              <img
+                class="object-cover object-top"
+                src="./assets/photo2.jpg"
+                alt="user"
+              />
+            </div>
+            <div class="card-position flex-shrink-0 pl-1">
+              <span class="text-xs font-pt-serif uppercase">Art Director</span>
+            </div>
+          </div>
+          <h4 class="text-lg md:text-xl font-semibold mt-2">Saba Cabrera</h4>
+        </div>
+      </div>
+
+      <div class="block">
+        <div
+          class="transform transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer"
+        >
+          <div class="card flex">
+            <div class="flex-1 aspect-w-4 aspect-h-5">
+              <img
+                class="object-cover object-top"
+                src="./assets/photo3.jpg"
+                alt="user"
+              />
+            </div>
+            <div class="card-position flex-shrink-0 pl-1">
+              <span class="text-xs font-pt-serif uppercase">Techlead</span>
+            </div>
+          </div>
+          <h4 class="text-lg md:text-xl font-semibold mt-2">Shae Le</h4>
+        </div>
+      </div>
+
+      <div class="block">
+        <div
+          class="transform transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer"
+        >
+          <div class="card flex">
+            <div class="flex-1 aspect-w-4 aspect-h-5">
+              <img
+                class="object-cover object-top"
+                src="./assets/photo4.jpg"
+                alt="user"
+              />
+            </div>
+            <div class="card-position flex-shrink-0 pl-1">
+              <span class="text-xs font-pt-serif uppercase">UX Designer</span>
+            </div>
+          </div>
+          <h4 class="text-lg md:text-xl font-semibold mt-2">Skylah Lu</h4>
+        </div>
+      </div>
+
+      <div class="block">
+        <div
+          class="transform transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer"
+        >
+          <div class="card flex">
+            <div class="flex-1 aspect-w-4 aspect-h-5">
+              <img
+                class="object-cover object-top"
+                src="./assets/photo5.jpg"
+                alt="user"
+              />
+            </div>
+            <div class="card-position flex-shrink-0 pl-1">
+              <span class="text-xs font-pt-serif uppercase">Developer</span>
+            </div>
+          </div>
+          <h4 class="text-lg md:text-xl font-semibold mt-2">Griff Richards</h4>
+        </div>
+      </div>
+
+      <div class="block">
+        <div
+          class="transform transition-all ease-in-out duration-500 hover:scale-110 cursor-pointer"
+        >
+          <div class="card flex">
+            <div class="flex-1 aspect-w-4 aspect-h-5">
+              <img
+                class="object-cover object-top"
+                src="./assets/photo6.jpg"
+                alt="user"
+              />
+            </div>
+            <div class="card-position flex-shrink-0 pl-1">
+              <span class="text-xs font-pt-serif uppercase">Develoepr</span>
+            </div>
+          </div>
+          <h4 class="text-lg md:text-xl font-semibold mt-2">Stan John</h4>
+        </div>
+      </div>
+    </div>
+    <div class="text-center pt-8" style="color: #a9a9a9">
+      assoft@DevChallenges.io
+    </div>
+  </div>
+</template>
